183f4a948ac2c51d524aa1e21779f5f08f3f323b
Ethan N <thanhnx.605@gmail.com>
2015-10-14 16:21:06 +0700

    Add flipclockjs

---
+++ libs/flipclockjs/flipclock.extended.js
@@ -0,0 +1,27 @@
+(function($) {
+    
+  /**
+   * FlipClock Swedish Language Pack
+   *
+   * This class will used to translate tokens into the Swedish language.
+   *  
+   */
+   
+  FlipClock.Lang.Vietnamese = {
+    
+    'years'   : 'Năm',
+    'months'  : 'Tháng',
+    'days'    : 'Ngày',
+    'hours'   : 'Giờ',
+    'minutes' : 'Phút',
+    'seconds' : 'Giây'  
+
+  };
+  
+  /* Create various aliases for convenience */
+
+  FlipClock.Lang['vi']      = FlipClock.Lang.Vietnamese;
+  FlipClock.Lang['vi-vn']   = FlipClock.Lang.Vietnamese;
+  FlipClock.Lang['vietnamese'] = FlipClock.Lang.Vietnamese;
+
+}(jQuery));
